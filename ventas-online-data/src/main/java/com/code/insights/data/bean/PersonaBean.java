3e08dc812433d5abc0cadfa249e32d6bd347d97e
Manuel Vega <sistemasmvu@gmail.com>
2015-12-02 14:24:22 -0500

    1.0 commit de Prueba

---
+++ ventas-online-data/src/main/java/com/code/insights/data/bean/PersonaBean.java
@@ -0,0 +1,10 @@
+package com.code.insights.data.bean;
+
+public class PersonaBean {
+    private String nombre;
+    private String apPaterno;
+    private String apMaterno;
+    private String dni;
+    
+    
+}
